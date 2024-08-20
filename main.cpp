7fcd9c9db3fcfddce3aaab69b8dbd4c225820f6a
AspidiNTON <57944727+AspidiNTON@users.noreply.github.com>
2024-08-20 14:35:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,72 +10,61 @@ enum numberOfSolutions {
     INFINITE
 };
 
-struct polynom {
-    double a, b, c, d, x1, x2;
-    numberOfSolutions n;
-};
 
-
-
-void readQuadratic(polynom *p) {
+void readQuadratic(double *a, double *b, double *c) {
     printf("Enter 3 multipliers of the quadratic equation:\n");
-    scanf("%lf %lf %lf", &(p->a), &(p->b), &(p->c));
+    scanf("%lf %lf %lf", a, b, c);
 }
 
-void printQuadratic(polynom *p) {
-    printf("Equation: %lfx^2 + %lfx + %lf = 0\n", p->a, p->b, p->c);
-}
-
-void solveLinear(polynom *p) {
-    if (p->b == 0) {
-        if (p->c == 0) p->n = INFINITE;
-        else p->n = ZERO;
+numberOfSolutions solveLinear(double p, double q, double* x) {
+    if (p == 0) {
+        return (q == 0) ? INFINITE : ZERO;
     } else {
-        p->n = ONE;
-        p->x1 = - p->c / p->b;
+        *x = - q / p;
+        return ONE;
     }
 }
 
-void solveQuadratic(polynom *p) {
-    if (p->a == 0) {
-        solveLinear(p);
+numberOfSolutions solveQuadratic(double a, double b, double c, double* x1, double* x2) {
+    if (a == 0) {
+        return solveLinear(b, c, x1);
     } else {
-        p->d = p->b * p->b - 4 * p->a * p->c;
-        if (p->d < -EPS) p->n = ZERO;
-        else if (p->d <= EPS) {
-            p->n = ONE;
-            p->x1 = -p->b / (2 * p->a);
+        double d = b * b - 4 * a * c;
+        if (d < -EPS) return ZERO;
+        else if (d <= EPS) {
+            *x1 = -b / (2 * a);
+            return ONE;
         }
         else {
-            p->n = TWO;
-            p->x1 = -(p->b + sqrt(p->d)) / (2 * p->a);
-            p->x2 = (sqrt(p->d) - p->b) / (2 * p->a);
+            *x1 = -(b + sqrt(d)) / (2 * a);
+            *x2 = (sqrt(d) - b) / (2 * a);
+            return TWO;
         }
     }
 }
 
 
-void printQuadraticSolutions(polynom *p) {
-    switch (p->n) {
+void printQuadraticSolutions(numberOfSolutions n, double x1, double x2) {
+    switch (n) {
         case ZERO:
             printf("The equation has no solutions\n");
             break;
         case ONE:
-            printf("The only root of the equation is: %lf\n", p->x1);
+            printf("The only root of the equation is: %lf\n", x1);
             break;
         case TWO:
-            printf("The equation has 2 solutions: %lf and %lf\n", p->x1, p->x2);
+            printf("The equation has 2 solutions: %lf and %lf\n", x1, x2);
             break;
         case INFINITE:
-            printf("The equation has no solutions\n");
+            printf("The equation has an infinite number of solutions\n");
             break;
     }
 }
 
 int main() {
-    polynom p;
-    readQuadratic(&p);
-    //printPolynom(&p);
-    solveQuadratic(&p);
-    printQuadraticSolutions(&p);
+    double a, b, c, x1, x2;
+    numberOfSolutions n;
+    readQuadratic(&a, &b, &c);
+    n = solveQuadratic(a, b, c, &x1, &x2);
+    printQuadraticSolutions(n, x1, x2);
 }
